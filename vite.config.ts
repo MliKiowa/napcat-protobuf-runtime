481415409ed19fba6610e2c4e53c4692b23ecd86
手瓜一十雪 <nanaeonn@outlook.com>
2025-02-20 14:01:11 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,23 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import cp from 'vite-plugin-cp';
+import { builtinModules } from 'node:module';
+const external = ['@protobuf-ts/runtime'];
+
+const nodeModules = [...builtinModules, builtinModules.map(m => `node:${m}`)].flat();
 
 export default defineConfig({
     build: {
         sourcemap: true,
         lib: {
-            entry: './src/protobuf.ts',
+            entry: './src/test.ts',
             name: 'protobuf',
             formats: ['es', 'cjs'],
             fileName: 'protobuf',
         },
+        rollupOptions: {
+            external: [...nodeModules, ...external]
+        }
     },
     plugins: [
         nodeResolve(),
